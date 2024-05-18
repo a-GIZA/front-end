9cce3710cc8c9793d7bd3fa80f8fa671b295add7
rasuliomari <rasuli707046@gmail.com>
2024-05-18 16:08:41 +0300

    updates

--- aGIZA/app/index.jsx
+++ aGIZA/app/index.jsx
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+
+import { images } from '../constants'
 
 export default function App()  {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className= "text-3xl font-pbblack">aGIZA</Text>
-      <StatusBar style="auto" />
-      <Link href="/home">Go to Home</Link>
-    </View>
+    <SafeAreaView classname="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%'}}>
+        <View classname="w-full justify-center items-center h-full px-4">
+          <Image 
+            source={images.aGIZA}
+            classname="w-[130px] h-[84px]"
+            resizeMode="contai"
+          />
+          <Image 
+            source={images.undraw_on_the_way_re_swjt}
+            classname="w-[100%] h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
